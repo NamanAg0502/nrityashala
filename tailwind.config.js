f648be5ac564fa9891368c7baa71c18cbed56cd3
Naman Agrawal <naman@Namans-MacBook-Air.local>
2023-07-04 00:58:00 +0530

    dark mode off

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export const content = [
   './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   './src/app/**/*.{js,ts,jsx,tsx,mdx}',
 ];
+export const darkMode = false; // or 'media' or 'class'
 export const theme = {
   extend: {
     colors: {
